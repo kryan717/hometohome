9e8cad96b8fa977ea9ee08bb12daf6903d810bdb
AbhinavPrasanna <abhinav2k01@gmail.com>
2021-03-13 11:47:07 -0800

    Changed Listing.java

--- app/src/main/java/com/example/home_to_home/listings.java
+++ app/src/main/java/com/example/home_to_home/listings.java
@@ -1,10 +1,10 @@
 package com.example.home_to_home;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
