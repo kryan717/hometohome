110cb38982fdf482727fd603e7bfbd248425b706
5852Alan <alanatriano@gmail.com>
2021-03-13 12:57:22 -0800

    Added trade interface.xml skeleton and blank tradeInterface java file for testing

---
+++ app/src/main/java/com/example/home_to_home/tradeInterface.java
@@ -0,0 +1,14 @@
+package com.example.home_to_home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class tradeInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trade_interface);
+    }
+}
