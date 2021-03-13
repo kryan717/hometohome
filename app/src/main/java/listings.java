4ad901c6b615449ddd7de125d6bfeaf77c9a6996
AbhinavPrasanna <abhinav2k01@gmail.com>
2021-03-13 09:44:32 -0800

    Started Implemention of Listing

--- app/src/main/java/listings.java
+++ app/src/main/java/listings.java
@@ -1,10 +1,10 @@
+package com.example.home_to_home;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import com.example.home_to_home.R;
 
 import java.util.ArrayList;
 import java.util.List;
