7d2177996ecab5e700d514de2e1bfc1b15ea7042
Aanchal116 <48170750+Aanchal116@users.noreply.github.com>
2021-03-13 21:47:47 +0530

    Added Background

--- app/src/main/java/com/example/home_to_home/LoginAccount.java
+++ app/src/main/java/com/example/home_to_home/LoginAccount.java
@@ -1,4 +1,30 @@
 package com.example.home_to_home;
 
-public class LoginAccount {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_account);
+
+        final Button button = (Button)findViewById(R.id.create_acc_btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginAccount.this, CreateAccount.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
 }
--- app/src/main/java/com/example/home_to_home/SetLocation.java
+++ app/src/main/java/com/example/home_to_home/SetLocation.java
@@ -2,7 +2,10 @@ package com.example.home_to_home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SetLocation extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class SetLocation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_location);
+
+        final Button button = (Button)findViewById(R.id.loc_btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
