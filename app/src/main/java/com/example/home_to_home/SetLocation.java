b313a6a1a91098e46214b1b5312c315b317f8086
Kyle Ryan <42781093+kryan717@users.noreply.github.com>
2021-03-13 13:59:06 -0500

    fixed some issues with set location xml file

--- app/src/main/java/com/example/home_to_home/SetLocation.java
+++ app/src/main/java/com/example/home_to_home/SetLocation.java
@@ -2,26 +2,46 @@ package com.example.home_to_home;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class SetLocation extends AppCompatActivity {
+
+    private EditText city, state, zipcode, country;
+
+    private String userID;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_location);
 
-        final Button button = (Button)findViewById(R.id.loc_btn);
+        city = (EditText) findViewById(R.id.city);
+        state = (EditText) findViewById(R.id.state);
+        zipcode = (EditText) findViewById(R.id.country);
+        country = (EditText) findViewById(R.id.zip);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        mAuth = FirebaseAuth.getInstance();
 
-            }
-        });
+    }
+
+    public void setLocationBtn(View view){
+        String zipcode_db = zipcode.getText().toString().trim();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        userID = user.getUid();
 
 
+        db.collection("users").document(this.userID).update("zipcode", zipcode_db);
     }
 }
