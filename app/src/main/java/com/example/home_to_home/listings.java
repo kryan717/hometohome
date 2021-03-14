99de41f04e808ff3cc74eaf4974514a1eb536e1a
5852Alan <alanatriano@gmail.com>
2021-03-13 21:56:43 -0800

    Fixed issue with ID for listings

--- app/src/main/java/com/example/home_to_home/listings.java
+++ app/src/main/java/com/example/home_to_home/listings.java
@@ -1,55 +1,98 @@
 package com.example.home_to_home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.firestore.CollectionReference;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
-public class listings extends AppCompatActivity {
+public class listings extends AppCompatActivity  {
 
+    private FirebaseAuth data1;
+    private FirebaseFirestore data;
     ListView listView;
-    List list = new ArrayList();
+    private List<String> items = new ArrayList<String>();
     ArrayAdapter adapter;
+    private Button button;
+    EditText name;
+    String address;
+    private String product;
+    User A;
+    String UserID;
+    private String user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_listings);
 
-        listView = (ListView)findViewById(R.id.tradeListView);
-        list.add("test");
-        list.add("test");
-        list.add("test");
-        list.add("test");
-        list.add("test");
-        list.add("test");
-        list.add("test");
-        list.add("test");
-        list.add("test");
-        list.add("test");
-        list.add("pizza");
-        list.add("test");
-        list.add("test");
-        list.add("test");
-        list.add("test");
+        button = (Button)findViewById(R.id.searchButton);
 
-        adapter = new ArrayAdapter(listings.this, android.R.layout.simple_list_item_1, list);
-        listView.setAdapter(adapter);
-    }
+        name = (EditText)findViewById(R.id.searchName);
 
-    public Query getListings(int zipcode){
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        CollectionReference users = db.collection("users");
-        Query usersInZip = users.whereEqualTo("zipcode", zipcode);
-        return usersInZip;
+
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                address = name.getText().toString();
+                data = FirebaseFirestore.getInstance();
+                data1 = FirebaseAuth.getInstance();
+
+                String UserID = data1.getCurrentUser().getUid();
+                user = data.collection("name").document(UserID).toString();
+
+                data.collection("users")
+                        .whereEqualTo("zipcode",address)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task){
+                                if(task.isSuccessful()) {
+                                    for(QueryDocumentSnapshot document: task.getResult()){
+                                        A = new User(document.get("email").toString(),
+                                                document.get("name").toString(), document.get("phoneNumber").toString());
+                                        items.add(document.get("items").toString());
+                                    }
+                                }
+
+                            }
+                        });
+                listView = (ListView)findViewById(R.id.tradeListView);
+                adapter = new ArrayAdapter(listings.this, android.R.layout.simple_list_item_1, items);
+                listView.setAdapter(adapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+                {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapter, View v, int position,
+                                            long arg3)
+                    {
+                        product=items.get(position);
+                        Intent intent = new Intent(listings.this,details.class);
+                        intent.putExtra("Item",items.get(position));
+                        intent.putExtra("owner",A.getName());
+                        intent.putExtra("email",A.getUsername());
+                        intent.putExtra("Zipcode",address);
+                    }
+                });
+            }
+        });
     }
 }
